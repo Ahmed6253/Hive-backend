ba0a5d9f84c8c08d57e79cd8958621f6a413c4b7
Ahmed6253 <abdelmonaem535@gmail.com>
2026-04-25 21:11:40 +0300

    Add 'get' route to group management API

--- src/modules/groups/group.routes.js
+++ src/modules/groups/group.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
-import { create, remove, edit } from "./group.controller.js";
+import { create, remove, edit, get } from "./group.controller.js";
 
 const groupRoutes = Router();
 
+groupRoutes.post("/get", get);
 groupRoutes.post("/create", create);
 groupRoutes.delete("/delete", remove);
 groupRoutes.put("/edit", edit);
